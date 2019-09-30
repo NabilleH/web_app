04691368b8ff971c36fcfe3608a2379217fd1b42
Nabille Helal <nabilleh@outlook.com>
2019-09-30 16:05:15 +0100

    First commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+gem 'shotgun'
+gem 'sinatra'
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
---
+++ lib/app.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+
+get '/' do
+  "Hello"
+end
+
+get '/secret' do
+  "Jay is superman and so is Nabille.. actually not"
+end
+
+get '/ping_pong' do
+  "Play ping pong"
+end
+
+get '/ping_pong' do
+  "Play ping pong again"
+end
+
+get '/cat' do
+  "<div style='border: 8px solid blue'>
+     <img src='http://bit.ly/1eze8aE'>
+   </div>"
+end
